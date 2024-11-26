c876c23c5b092320fe1f80b0c7c61b7982537738
Andrew Park <hep003@ucsd.edu>
2024-11-26 10:59:39 -0800

    PR #1: Add Home Page (#1)
    * init home page:
    
    * add style
    
    * add background

---
+++ frontend/app/page.jsx
@@ -0,0 +1,19 @@
+import style from "./page.module.css";
+
+export default function Page() {
+  return (
+    <div className={style.container}>
+      <div className={style.topRight}>
+        <button className={style.signInButton}>Login</button>
+        <button className={style.signInButton}>Signup</button>
+      </div>
+      <h1 className={style.title}>Flight Search</h1>
+      <input
+        className={style.searchBar}
+        type="text"
+        placeholder="Enter destination"
+      />
+      <button className={style.searchButton}>Search</button>
+    </div>
+  );
+}
