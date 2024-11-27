f89f15f42fc7da9438677bde1322aef95f45042e
7174Andy <hep003@ucsd.edu>
2024-11-27 10:55:54 -0800

    add states

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -2,9 +2,13 @@
 
 import { useRouter } from "next/navigation";
 import style from "./home.module.css";
+import { useState } from "react";
 
 export default function Page() {
   const router = useRouter();
+
+  const [departure, setDeparture] = useState("");
+  const [destination, setDestination] = useState("");
 
   return (
     <div className={style.container}>
@@ -21,8 +25,16 @@ export default function Page() {
       <input
         className={style.searchBar}
         type="text"
-        placeholder="Enter destination"
+        placeholder="Enter departure"
+        onChange={(e) => setDeparture(e.target.value)}
       />
+      <input
+        className={style.searchBar}
+        type="text"
+        placeholder="Enter destination"
+        onChange={(e) => setDestination(e.target.value)}
+      />
+
       <button className={style.searchButton}>Search</button>
     </div>
   );
