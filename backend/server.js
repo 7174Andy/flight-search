ec2dcdb84aa8062e801f6c3859a2dd80a7fe7ac1
7174Andy <hep003@ucsd.edu>
2024-11-27 16:38:22 -0800

    add resolvers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
 import mongoose from "mongoose";
 import { gql, ApolloServer } from "apollo-server";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 dotenv.config({ path: "./.env" });
 
 const uri = process.env.MONGO_DB_URI;
 
+// Mongoose Schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+// GraphQL
 const typeDefs = gql`
   type Query {
     hello: String
@@ -27,25 +45,56 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => "Hello, world!",
+    allUsers: async () => {
+      const users = await User.find();
+      return users;
+    },
+  },
+
+  Mutation: {
+    login: async (_, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      const valid = await bcrypt.compare(password, user.password);
+
+      if (!valid) {
+        throw new Error("Invalid password");
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+      return { token };
+    },
+
+    register: async (_, { username, password }) => {
+      if (!username || !password) {
+        throw new Error("Invalid input");
+      }
+
+      const existingUser = await User.findOne({
+        username,
+      });
+      if (existingUser) {
+        throw new Error("User already exists");
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({
+        username,
+        password: hashedPassword,
+      });
+      await user.save();
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+      return { token };
+    },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
-
-// Mongoose Schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-const User = mongoose.model("User", userSchema);
 
 mongoose
   .connect(uri, { useNewUrlParser: true })
